a356fed37ed894860aec806b80bbc0dfefd00bb5
Stephen McCallion <s.mccallion@10ticks.co.uk>
2022-11-18 17:07:49 +0000

    Added dictionary of params to view model

--- DeptTechnicaTest-OPenAQ/Models/ViewModels/City.cs
+++ DeptTechnicaTest-OPenAQ/Models/ViewModels/City.cs
@@ -5,13 +5,28 @@ namespace DeptTechnicaTest_OPenAQ.Models.ViewModels
 {
 	public class City
 	{
-        public CityRoot cities { get; set; }
-
+		public CityRoot cities { get; set; }
+		public Dictionary<string, string> cityParams {get; set;}
         public City(CityRoot city)
 		{
 			this.cities = city;
-		}
+			populateCityParam();
 
-	}
+        }
+
+		private void populateCityParam()
+		{
+			cityParams= new Dictionary<string, string>();
+			cityParams.Add("Limit", "");
+            cityParams.Add("Page", "");
+            cityParams.Add("Offset", "");
+            cityParams.Add("Countryid", "");
+            cityParams.Add("Country", "");
+            cityParams.Add("City", "");
+            cityParams.Add("Order-By", "");
+            cityParams.Add("Entity", "");
+        }
+
+    }
 }
 
